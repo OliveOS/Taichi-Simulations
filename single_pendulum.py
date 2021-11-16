b9802a411d20d5ecf179c66f4a10af142db22ff6
oliverwooding <oliverwooding@icloud.com>
2021-11-16 22:55:01 +0100

    generic

--- single_pendulum.py
+++ single_pendulum.py
@@ -13,12 +13,17 @@ pivot = ti.Vector([0.5, 0.5])
 
 nbodies: int = 1 #total pendulums in field
 pendulum_field = ti.Struct.field({
-    "length": ti.types.vector(2, ti.f32),
+    "length": ti.f32,
     "pos": ti.types.vector(2, ti.f32),
     "vel": ti.types.vector(2, ti.f32),
     "acc": ti.types.vector(2, ti.f32),
     "mass": ti.f32,
 }, shape=(nbodies,))
+
+pendulum_field[0].length = 0.3
+pendulum_field[0].pos = ti.Vector([0.8, 0.5])
+pendulum_field[0].mass = 1
+
 
 @ti.func
 def get_equilibrium(r):
@@ -30,16 +35,30 @@ def get_theta(eq, pos, r):
     return ti.asin(b/r)
 
 @ti.func
-def update_pos_derivatives(theta, dt):
-    for pendulum in range(pendulum_field):
+def update_pos_derivatives(dt):
+    for i in range(pendulum_field):
+
+        theta = get_theta(get_equilibrium(pendulum_field[i].length), pendulum_field[i].pos, pendulum_field[i].length)
+
         #set accelerations (not dependent on dt or previous values of acceleration)
-        pendulum.acc[0] = g * ti.sin(theta) * ti.cos(theta)
-        pendulum.acc[1] = g * ti.sin(theta)**2
+        pendulum_field[i].acc[0] = g * ti.sin(theta) * ti.cos(theta)
+        pendulum_field[i].acc[1] = g * ti.sin(theta)**2
 
         #set velocities
-        pendulum.vel[0] = pendulum.acc[0] * dt
-        pendulum.vel[1] = pendulum.acc[1] * dt
+        pendulum_field[i].vel[0] = pendulum_field[i].acc[0] * dt
+        pendulum_field[i].vel[1] = pendulum_field[i].acc[1] * dt
 
         #set positions
-        pendulum.pos[0] = pendulum.vel[0] * dt
-        pendulum.pos[1] = pendulum.vel[1] * dt
+        pendulum_field[i].pos[0] = pendulum_field[i].vel[0] * dt
+        pendulum_field[i].pos[1] = pendulum_field[i].vel[1] * dt
+
+@ti.kernel 
+def update(dt: float):
+    update_pos_derivatives(dt)
+
+#setup gui details (name and x by y resolution)
+gui = ti.GUI("Pendulum", res=(n, n))
+
+for i in range(10000):
+    update(0.01)
+    gui.show()
