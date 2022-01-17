5fcb8f5a576ecc74f16893ac8b81d02581775e14
Oliver Wooding <oliverwooding@icloud.com>
2022-01-17 22:48:46 +0100

    added polar_to_cartesian function

--- double_pendulum.py
+++ double_pendulum.py
@@ -7,11 +7,12 @@ pixels = ti.field(dtype=float, shape=(n, n)) #square screen
 
 #simulation constants
 g: float = 9.81 #gravitational constant
+root: ti.Vector([n/2, 0.75*n])
 
 nbodies: int = 2 #total pendulums in field
 pendulum_field = ti.Struct.field({
     "length": ti.f32,
-    "pos": ti.Vector(2, dt=ti.f32),
+    "pos": ti.types.vector(2, dt=ti.f32),
     "theta": ti.f32, #in degrees
     "omega": ti.f32,
     "mass": ti.f32,
@@ -28,9 +29,10 @@ pendulum_field[0].mass = 1
 pendulum_field[1].mass = 1
 
 def polar_to_cartesian(r, theta):
-    pass
+    return ti.Vector([r*ti.cos(theta), r*ti.sin(theta)])
 
-#TODO:Calculate remaining initial state of pendula
+#set initial position of pendula
+
 #TODO:Write update-omega function
 #TODO:Write update-theta function
 #TODO:Resolve functions into kernel-scope
