423a53f4666dddcd16561e8292b32a303570f92f
Oliver Wooding <oliverwooding@icloud.com>
2022-01-18 11:26:20 +0100

    updated double_pendulum.py (bugged on macos)

--- double_pendulum.py
+++ double_pendulum.py
@@ -1,3 +1,4 @@
+from math import pi
 import taichi as ti
 
 #init taichi context + window details
@@ -7,32 +8,59 @@ pixels = ti.field(dtype=float, shape=(n, n)) #square screen
 
 #simulation constants
 g: float = 9.81 #gravitational constant
-root: ti.Vector([n/2, 0.75*n])
+root = ti.Vector([0.5, 0.7])
+
+#!!! TAICHI TRIG FUNCTIONS OPERATE ON RADIANS
 
 nbodies: int = 2 #total pendulums in field
 pendulum_field = ti.Struct.field({
     "length": ti.f32,
-    "pos": ti.types.vector(2, dt=ti.f32),
-    "theta": ti.f32, #in degrees
+    "pos": ti.types.vector(2, ti.f32),
+    "theta": ti.f32, #in radians
     "omega": ti.f32,
     "mass": ti.f32,
 }, shape=(nbodies,))
 
-#set initial independent state
-pendulum_field[0].length = 1
-pendulum_field[1].length = 0.7
-pendulum_field[0].theta = -90
-pendulum_field[1].theta = 20
-pendulum_field[0].omega = 0
-pendulum_field[1].omega = 0
+#initialize independent state
 pendulum_field[0].mass = 1
 pendulum_field[1].mass = 1
 
-def polar_to_cartesian(r, theta):
-    return ti.Vector([r*ti.cos(theta), r*ti.sin(theta)])
+pendulum_field[0].length = 1
+pendulum_field[1].length = 0.7
 
-#set initial position of pendula
+pendulum_field[0].theta = pi/2
+pendulum_field[1].theta = -pi/4
 
-#TODO:Write update-omega function
-#TODO:Write update-theta function
-#TODO:Resolve functions into kernel-scope
+#initalize dependent state
+pendulum_field[0].pos = root + ti.Vector([pendulum_field[0].length*ti.cos(pendulum_field[0].theta), pendulum_field[0].length*ti.sin(pendulum_field[0].theta)])
+pendulum_field[1].pos = pendulum_field[0].pos + ti.Vector([pendulum_field[1].length*ti.cos(pendulum_field[1].theta), pendulum_field[0].length*ti.sin(pendulum_field[1].theta)])
+
+@ti.func
+def alpha_1():
+    one = pendulum_field[0]
+    two = pendulum_field[1]
+    return (-g*(2*one.mass + two.mass)*ti.sin(one.theta)-two.mass*g*ti.sin(one.theta-2*two.theta)-2*ti.sin(one.theta-two.theta)*two.mass*(two.omega**2*two.length+one.omega**2*one.length*ti.cos(one.theta-two.theta)))/(one.length*(2*one.mass+two.mass-two.mass*ti.cos(2*one.theta-2*two.theta)))
+
+@ti.func
+def alpha_2():
+    one = pendulum_field[0]
+    two = pendulum_field[1]
+    return (2*ti.sin(one.theta-two.theta)*(one.omega**2*one.length*(one.mass+two.mass)+g*(one.mass+two.mass)*ti.cos(one.theta)+two.omega**2*two.length*two.mass*ti.cos(one.theta-two.theta)))/(one.length*(2*one.mass+two.mass-two.mass*ti.cos(2*one.theta-2*two.theta)))
+
+@ti.kernel
+def update(dt: float):
+    #using euler approximations to advance the simulation
+    pendulum_field[0].omega += alpha_1() * dt
+    pendulum_field[1].omega += alpha_2() * dt
+
+    pendulum_field[0].theta += pendulum_field[0].omega * dt
+    pendulum_field[1].theta += pendulum_field[1].omega * dt
+
+gui = ti.GUI("Double Pendulum", res=(n, n))
+
+for i in range(10000):
+    gui.circle(root, radius = 7, color=0x068587)
+    update(0.01)
+    for i in range(nbodies):
+        gui.circle(pendulum_field[i].pos, radius = 15, color=0x068587)
+    gui.show()
